1ab3804cd15d4d92b785955bc01aa905a498a1e8
Gaurav Jain <grvmjain@gmail.com>
2012-12-08 22:20:16 +0530

    Added error checking to kill-script.sh.

--- kill-script.sh
+++ kill-script.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-kill `ps aux | grep low-bat.sh | grep -v grep | awk '{print $2}'`
+low_bat_pid=`ps aux | grep low-bat.sh | grep -v grep | awk '{print $2}'`
+if [ -z $low_bat_pid ]
+then
+    echo "Low Battery Reminder is already stopped."
+else
+    kill $low_bat_pid
+    echo "Low Battery Reminder killed."
+fi
