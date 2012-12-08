a6b0d6138280f1e2ab71b9e9da5b1d3aa418353b
Gaurav Jain <grvmjain@gmail.com>
2012-12-09 00:17:46 +0530

    Updated kill-script.sh

--- kill-script.sh
+++ kill-script.sh
@@ -3,8 +3,8 @@
 low_bat_pid=`ps aux | grep low-bat.sh | grep -v grep | awk '{print $2}'`
 if [ -z $low_bat_pid ]
 then
-    echo "Low Battery Reminder is already stopped."
+    echo "Low Battery Reminder is not running."
 else
     kill $low_bat_pid
-    echo "Low Battery Reminder killed."
+    echo "Low Battery Reminder terminated."
 fi
